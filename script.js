b3da733602be3765e8a9766df09c3290942e44d0
Pranav Raju <120582474+pranavraju1@users.noreply.github.com>
2023-10-08 14:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function createTable() {
     //Write your code here
 	  const table = document.getElementById("myTable");
-  let TR = prompt("input number of rows");
-  let TD = prompt("input number of columns");
+  let TR = prompt("Input number of rows");
+  let TD = prompt("Input number of columns");
   for (let i = 0; i < TR; i++) {
     const tr = document.createElement("tr");
     for (let j = 0; j < TD; j++) {
